7f19444ef3a597614a346e548911fe2e14633986
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-07-01 18:45:04 +0000

    Add createVisitor GraphQL mutation
    Implements the new `createVisitor` mutation as requested in issue # 30.
    
    The mutation accepts the following arguments:
    - name (required string)
    - lastname (required string)
    - phone (required string)
    - companyName (required string)
    - companyRole (required string)
    
    And returns a VisitorType with the same fields.
    
    This implementation only affects the GraphQL layer as requested, without service layer integration. The resolver currently returns the input values as output.
    
    Changes made:
    - Added VisitorType to backend/domain/gql/types/types.go
    - Added CreateVisitorField resolver to backend/domain/gql/fields/fields.go
    - Added createVisitor mutation to backend/domain/gql/operations/operations.go
    
    Closes # 30
    
    Co-authored-by: chris-ramon <1000404+chris-ramon@users.noreply.github.com>

--- backend/domain/gql/fields/fields.go
+++ backend/domain/gql/fields/fields.go
@@ -97,3 +97,61 @@ var UsersField = &graphql.Field{
 		return usersAPI, nil
 	},
 }
+
+var CreateVisitorField = &graphql.Field{
+	Type:        types.VisitorType,
+	Description: "Creates a new visitor.",
+	Args: graphql.FieldConfigArgument{
+		"name": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"lastname": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"phone": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"companyName": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"companyRole": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+	},
+	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+		name, err := fieldFromArgs[string](p.Args, "name")
+		if err != nil {
+			return nil, err
+		}
+
+		lastname, err := fieldFromArgs[string](p.Args, "lastname")
+		if err != nil {
+			return nil, err
+		}
+
+		phone, err := fieldFromArgs[string](p.Args, "phone")
+		if err != nil {
+			return nil, err
+		}
+
+		companyName, err := fieldFromArgs[string](p.Args, "companyName")
+		if err != nil {
+			return nil, err
+		}
+
+		companyRole, err := fieldFromArgs[string](p.Args, "companyRole")
+		if err != nil {
+			return nil, err
+		}
+
+		visitor := map[string]interface{}{
+			"name":        name,
+			"lastname":    lastname,
+			"phone":       phone,
+			"companyName": companyName,
+			"companyRole": companyRole,
+		}
+
+		return visitor, nil
+	},
+}
