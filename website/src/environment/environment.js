2b1ab7141c873f8a5a2d77768d58a96577f4bffe
Chris Ramón <rchristian.ramon@gmail.com>
2025-07-03 18:09:44 -0500

    src/environment: adds graphql base urls

---
+++ website/src/environment/environment.js
@@ -0,0 +1,8 @@
+export const environment = {
+  gateway: {
+    graphqlURL: {
+      development: "http://localhost:8080/graphql",
+      production: "https://gateway-segguro-com.onrender.com/graphql",
+    },
+  },
+};
